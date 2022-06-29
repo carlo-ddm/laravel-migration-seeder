bd5d48846d8237a3f39fda17d063051237bb26fa
carlo-ddm <carloduminuco90@gmail.com>
2022-06-29 20:09:57 +0200

    ho dimenticato di inserire 'azienda' -> reinserita con upgrade

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -1,17 +1,16 @@
 <?php
-use App\Train;
-use Faker\Generator as Faker;
+
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class TrainsTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-
+        //
     }
 }
