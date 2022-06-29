15baf8337c1f0a456ec5955b1e7591511eb56f92
carlo-ddm <carloduminuco90@gmail.com>
2022-06-29 18:37:21 +0200

    intallazione boostrap / creazione layout / creazione controller pagina statica (PageController) / connessione rotte/ creazione migration 'trains'

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel-migration-seeder*</title>
+</head>
+<body>
+    {{-- Fase 1
+Creiamo una tabella **trains** e relativa **Migration**
+Ogni treno dovrà avere:
+Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato
+Fase 2
+Creiamo un seeder della tabella.
+Visto che a lezione il Faker è stato visto un po’ di corsa è possibile popolare la tabella con dei dati presenti un un array che prepariamo e che cicliamo.
+Alternativamente (BONUS) popolare la tabella con dati generati dal Faker --}}
+@yield('content')
+</body>
+</html>
